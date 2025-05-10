19997448bc33fadde69f93a461c3acf04f12b54d
WinterDust <youssefelajbari@gmail.com>
2025-05-10 15:51:06 +0100

    Update PrimeCheck.py

--- PrimeCheck.py
+++ PrimeCheck.py
@@ -5,3 +5,40 @@ def PrimeCheck(number):
             return False
         a+=1
     return True
+def f(mn, mx):
+    return 2*((2 * mn * mx) + mn + mx)+1
+
+
+def a(c):
+    r = int(((c**0.5)-1)/2)
+    if r<1:
+        return 1
+    return r
+def PrimeCheck2(n):
+    if n < 3:
+        return
+
+    if n%2 == 0:
+        return False
+    index= [1, a(n)] # (min, max)
+    while True:
+
+        k = f(index[0], index[1])
+        if n == k :
+            return False
+        
+        elif n > k:
+
+            if index[0] == index [1] :
+                index[0] , index[1] = 1 , index[1] + 1
+            
+            else:
+                index[0] = index[0] + 1
+        
+        else:
+
+            if index[0] == 1:
+                return True
+            
+            else:
+                index[0] , index[1] = 1 , index[1] + 1
